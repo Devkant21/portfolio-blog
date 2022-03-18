05f174d6585a66714eea884647ab4d0554809749
Devkant <devkant21@gmail.com>
2022-03-18 10:34:05 +0530

    NavBar not loading

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import Project from "./components/Project"
 
 const App = () => {
     return (
+        <>
         <BrowserRouter>
             <Routes>
-                <Route component={Home} path="/" exact />
-                <Route component={About} path="/about" />
-                <Route component={SinglePost} path="/post/:slug" />
-                <Route component={Post} path="/post" />
-                <Route component={Project} path="/project" />
+                <Route component={Home} path='/' exact />
+                <Route component={About} path='/about' />
+                <Route component={SinglePost} path='/post/:slug' />
+                <Route component={Post} path='/post' />
+                <Route component={Project} path='/project' />
             </Routes>
-
         </BrowserRouter>
-        
+
+        <h1>Hello World</h1>
+        <button class="bg-red-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded ml-4 mt-4">Button</button>
+        </>
     );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,30 @@
-import { Navlink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
-
-export default function Navbar() {
+export default function NavBar() {
     return (
-        <header>
-            <div>
+        <header className="bg-red-600">
+            <div className="container mx-auto flex justify-between">
                 <nav>
-                    <Navlink to="/" exact>
+                    <NavLink to="/" exact>
                         Devkant
-                    </Navlink>
-                    <Navlink to="/post">
-                        BlogPost
-                    </Navlink>
-                    <Navlink to="/project">
+                    </NavLink>
+                    <NavLink to='/post'>
+                        Blog Post
+                    </NavLink>
+                    <NavLink to='/project'>
                         Projects
-                    </Navlink>
-                    <Navlink to="/about">
+                    </NavLink>
+                    <NavLink to='/about'>
                         About 
-                    </Navlink>
+                    </NavLink>
                 </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon url="./https://twitter.com/devkant21" className="mr-4" target="blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+                    <SocialIcon url="./https://github.com/Devkant21" className="mr-4" target="blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+                    <SocialIcon url="./https://dev21.hashnode.dev/" className="mr-4" target="blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+
+                </div>
+                
             </div>
         </header>
     )
